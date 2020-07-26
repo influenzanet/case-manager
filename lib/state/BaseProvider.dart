ac71156ca53b4c6735fd090203ab687553751d33
Kevin Brendel <kBrendel@hotmail.de>
2020-07-26 03:07:03 +0200

    Some cleanup.

--- lib/state/BaseProvider.dart
+++ lib/state/BaseProvider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// Derive all states from BaseState so features related to persisting state can be more easily added.
 abstract class BaseProvider extends ChangeNotifier {
   update() {
     notifyListeners();
