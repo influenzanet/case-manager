c4b969bb9fa08b8cbd94e601e15b260e25054bd6
Kevin Brendel <kBrendel@hotmail.de>
2020-07-30 13:13:30 +0200

    Removed nonfunctional code.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:case_manager/state/Storage.dart';
 import 'package:case_manager/AppModule.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:intl/intl_standalone.dart';
+// import 'package:intl/intl_standalone.dart';
 
 import 'api/Api.dart';
 import 'config/Config.dart';
@@ -14,7 +14,7 @@ void main() async {
 
 Future initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await findSystemLocale();
+  // await findSystemLocale();
   await Config.initialize();
   await Storage.initialize();
   await Api.initialize();
