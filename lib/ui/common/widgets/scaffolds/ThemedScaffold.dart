38dfde79dff0550051a5f1fa9908d21d35059d49
Kevin Brendel <kBrendel@hotmail.de>
2020-08-06 08:35:59 +0200

    Added watermark.

--- lib/ui/common/widgets/scaffolds/ThemedScaffold.dart
+++ lib/ui/common/widgets/scaffolds/ThemedScaffold.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:case_manager/ui/common/widgets/app-bars/ThemedAppBar.dart';
+import 'package:case_manager/ui/common/widgets/images/SvgImage.dart';
 import 'package:flutter/material.dart';
 
 class ThemedScaffold extends Scaffold {
@@ -16,11 +19,39 @@ class ThemedScaffold extends Scaffold {
             drawer: drawer,
             body: LayoutBuilder(
               builder: (BuildContext context, BoxConstraints viewportConstraints) {
-                return SingleChildScrollView(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(minHeight: viewportConstraints.maxHeight),
-                    child: body,
-                  ),
+                final appBarHeight = appBar.getPlannedHeight();
+                final screenHeight = viewportConstraints.maxHeight + appBarHeight;
+                final screenWidth = viewportConstraints.maxWidth;
+
+                final screenFactor = min(screenHeight, screenWidth);
+
+                final imageHeight = screenFactor * 2.1;
+                final imageWidth = imageHeight;
+
+                final excessHeight = imageHeight - screenHeight;
+                final excessWidth = imageWidth - screenWidth;
+
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Positioned.fromRect(
+                      rect: Rect.fromLTWH(Alignment.topLeft.x - excessWidth / 2,
+                          Alignment.topLeft.y - appBarHeight - excessHeight / 2, imageWidth, imageHeight),
+                      child: svgImageAsset(
+                        "images/logos/ConenoLogo.svg",
+                        color: Color(0xff606060),
+                        fit: BoxFit.fill,
+                        width: imageWidth,
+                        height: imageHeight,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(minHeight: viewportConstraints.maxHeight),
+                        child: body,
+                      ),
+                    ),
+                  ],
                 );
               },
             ));
